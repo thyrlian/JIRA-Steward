d510519e98583c05fac0cc9a64832aaebad205d2
Jing Li <thyrlian@gmail.com>
2022-08-08 19:43:06 +0200

    Enable logging for the Feign client

--- src/main/kotlin/com/basgeekball/jirasteward/client/FeignClientConfiguration.kt
+++ src/main/kotlin/com/basgeekball/jirasteward/client/FeignClientConfiguration.kt
@@ -1,12 +1,18 @@
 package com.basgeekball.jirasteward.client
 
 import com.basgeekball.jirasteward.properties.JiraProperties
+import feign.Logger
 import feign.auth.BasicAuthRequestInterceptor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class FeignClientConfiguration {
+    @Bean
+    fun feignLoggerLevel(): Logger.Level {
+        return Logger.Level.BASIC;
+    }
+
     @Bean
     fun basicAuthRequestInterceptor(jiraProperties: JiraProperties): BasicAuthRequestInterceptor {
         return BasicAuthRequestInterceptor(jiraProperties.auth.username, jiraProperties.auth.password)
