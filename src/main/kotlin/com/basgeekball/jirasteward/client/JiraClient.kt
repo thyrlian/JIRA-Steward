80058e8699a7470836293ead236f81b7ab16026f
Jing Li <thyrlian@gmail.com>
2022-08-13 14:18:16 +0200

    Add two more endpoints to JiraClient

--- src/main/kotlin/com/basgeekball/jirasteward/client/JiraClient.kt
+++ src/main/kotlin/com/basgeekball/jirasteward/client/JiraClient.kt
@@ -1,8 +1,8 @@
 package com.basgeekball.jirasteward.client
 
-import com.basgeekball.jirasteward.model.ServerInfo
-import com.basgeekball.jirasteward.model.Status
+import com.basgeekball.jirasteward.model.*
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
@@ -13,4 +13,10 @@ interface JiraClient {
 
     @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.common}/statuscategory"])
     fun getStatusCategory() : List<Status.StatusCategory>
+
+    @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.common}/project"])
+    fun getProjects() : List<Project>
+
+    @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.common}/issue/{issueKey}"])
+    fun getIssue(@PathVariable("issueKey") issueKey: String) : Issue
 }
