54c05e18135288893d90f592838fb4cd7057355f
Jing Li <thyrlian@gmail.com>
2022-08-14 12:11:27 +0200

    Make some properties in Fields model optional

--- src/main/kotlin/com/basgeekball/jirasteward/model/Fields.kt
+++ src/main/kotlin/com/basgeekball/jirasteward/model/Fields.kt
@@ -8,12 +8,12 @@ import java.util.*
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Fields(
         @JsonProperty("summary") val summary: String,
-        @JsonProperty("issuetype") val type: IssueType,
-        @JsonProperty("project") val project: Project,
-        @JsonProperty("priority") val priority: Priority,
+        @JsonProperty("issuetype") val type: IssueType?,
+        @JsonProperty("project") val project: Project?,
+        @JsonProperty("priority") val priority: Priority?,
         @JsonProperty("status") val status: Status,
-        @JsonProperty("created") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ") val created: Date,
-        @JsonProperty("updated") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ") val updated: Date,
+        @JsonProperty("created") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ") val created: Date?,
+        @JsonProperty("updated") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ") val updated: Date?,
         @JsonProperty("resolutiondate") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ") val resolved: Date?,
         @JsonProperty("reporter") val reporter: User?,
         @JsonProperty("assignee") val assignee: User?,
