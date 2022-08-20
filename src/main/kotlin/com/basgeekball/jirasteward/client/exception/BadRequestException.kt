133f0cf4819d0763d9c0a4935cb18b9095bfc774
Jing Li <thyrlian@gmail.com>
2022-08-20 22:38:58 +0200

    Remove empty constructor body in all custom exceptions

--- src/main/kotlin/com/basgeekball/jirasteward/client/exception/BadRequestException.kt
+++ src/main/kotlin/com/basgeekball/jirasteward/client/exception/BadRequestException.kt
@@ -1,9 +1,9 @@
 package com.basgeekball.jirasteward.client.exception
 
 class BadRequestException : Exception {
-    constructor() {}
-    constructor(message: String?) : super(message) {}
-    constructor(cause: Throwable?) : super(cause) {}
+    constructor()
+    constructor(message: String?) : super(message)
+    constructor(cause: Throwable?) : super(cause)
 
     override fun toString(): String {
         return "BadRequestException: $message"
--- src/main/kotlin/com/basgeekball/jirasteward/client/exception/NotFoundException.kt
+++ src/main/kotlin/com/basgeekball/jirasteward/client/exception/NotFoundException.kt
@@ -1,9 +1,9 @@
 package com.basgeekball.jirasteward.client.exception
 
 class NotFoundException : Exception {
-    constructor() {}
-    constructor(message: String?) : super(message) {}
-    constructor(cause: Throwable?) : super(cause) {}
+    constructor()
+    constructor(message: String?) : super(message)
+    constructor(cause: Throwable?) : super(cause)
 
     override fun toString(): String {
         return "NotFoundException: $message"
