f9b9a73c8c9bf0db799c557dc38a7a976492b952
Jing Li <thyrlian@gmail.com>
2022-08-21 00:54:13 +0200

    Add complex but awesome JQLBuilder (don't wanna split any logic, simply dump everything into this one)

---
+++ src/main/kotlin/com/basgeekball/jirasteward/domain/jql/IllegalClausesJoiningException.kt
@@ -0,0 +1,18 @@
+package com.basgeekball.jirasteward.domain.jql
+
+class IllegalClausesJoiningException(
+    clauses: MutableList<String>,
+    newClause: String,
+    message: String = frameMessage(clauses, newClause)
+) : Exception(message) {
+    companion object {
+        private fun frameMessage(clauses: MutableList<String>, newClause: String): String {
+            return """${System.lineSeparator()}
+                |   None of these join keywords: ${JQLBuilder.JOIN_KEYWORDS.values} are found.
+                |   The last clause is: "${clauses.last()}"
+                |   The new clause is: "$newClause"
+                |   The complete clauses are: ${clauses + newClause}
+            """.trimMargin()
+        }
+    }
+}
---
+++ src/main/kotlin/com/basgeekball/jirasteward/domain/jql/JQLBuilder.kt
@@ -0,0 +1,164 @@
+package com.basgeekball.jirasteward.domain.jql
+
+class JQLBuilder {
+    private var clauses = mutableListOf<String>()
+
+    companion object {
+        val JOIN_KEYWORDS: HashMap<String, String> = hashMapOf("and" to "AND", "or" to "OR")
+    }
+
+    //region JQL operators
+    // https://support.atlassian.com/jira-software-cloud/docs/advanced-search-reference-jql-operators/
+    // ================================================================================
+    private fun compare(
+        field: String,
+        operator: String,
+        vararg values: String,
+        valueInListForm: Boolean = false
+    ): JQLBuilder {
+        // compose clause
+        val mergedValues = if (values.isEmpty()) {
+            ""
+        } else if (values.size == 1 && !valueInListForm) {
+            values.first()
+        } else {
+            values.joinToString(prefix = "(", postfix = ")", separator = ",")
+        }
+        val newClause = "$field $operator" + if (mergedValues.isEmpty()) "" else " $mergedValues"
+        // validate
+        if (clauses.isNotEmpty() && !JOIN_KEYWORDS.containsValue(clauses.last())) {
+            throw IllegalClausesJoiningException(clauses, newClause)
+        }
+        clauses.add(newClause)
+        return this
+    }
+
+    fun equal(field: String, value: String): JQLBuilder {
+        return compare(field, "=", value)
+    }
+
+    fun notEqual(field: String, value: String): JQLBuilder {
+        return compare(field, "!=", value)
+    }
+
+    fun greaterThan(field: String, value: String): JQLBuilder {
+        return compare(field, ">", value)
+    }
+
+    fun greaterThanOrEqualTo(field: String, value: String): JQLBuilder {
+        return compare(field, ">=", value)
+    }
+
+    fun lessThan(field: String, value: String): JQLBuilder {
+        return compare(field, "<", value)
+    }
+
+    fun lessThanOrEqualTo(field: String, value: String): JQLBuilder {
+        return compare(field, "<=", value)
+    }
+
+    fun inEitherOf(field: String, vararg values: String): JQLBuilder {
+        return compare(field, "in", *values, valueInListForm = true)
+    }
+
+    fun notInAnyOf(field: String, vararg values: String): JQLBuilder {
+        return compare(field, "not in", *values, valueInListForm = true)
+    }
+
+    fun contain(field: String, value: String): JQLBuilder {
+        return compare(field, "~", value)
+    }
+
+    fun containWildcardMatch(field: String, value: String): JQLBuilder {
+        return compare(field, "~", "\"$value*\"")
+    }
+
+    fun containMultipleWords(field: String, vararg values: String): JQLBuilder {
+        return compare(field, "~", "\"${values.joinToString(" ")}\"")
+    }
+
+    fun containExactPhrase(field: String, value: String): JQLBuilder {
+        return compare(field, "~", "\"\\\"$value\\\"\"")
+    }
+
+    fun notContain(field: String, value: String): JQLBuilder {
+        return compare(field, "!~", value)
+    }
+
+    fun haveNoValue(field: String): JQLBuilder {
+        return compare(field, "is", "empty")
+    }
+
+    fun haveAValue(field: String): JQLBuilder {
+        return compare(field, "is not", "empty")
+    }
+
+    fun was(field: String, value: String): JQLBuilder {
+        return compare(field, "WAS", value)
+    }
+
+    fun wasIn(field: String, vararg values: String): JQLBuilder {
+        return compare(field, "WAS IN", *values, valueInListForm = true)
+    }
+
+    fun wasNotIn(field: String, vararg values: String): JQLBuilder {
+        return compare(field, "WAS NOT IN", *values, valueInListForm = true)
+    }
+
+    fun wasNot(field: String, value: String): JQLBuilder {
+        return compare(field, "WAS NOT", value)
+    }
+
+    fun changed(field: String): JQLBuilder {
+        return compare(field, "CHANGED")
+    }
+    // ================================================================================
+    //endregion
+
+    //region JQL keywords
+    // https://support.atlassian.com/jira-software-cloud/docs/advanced-search-reference-jql-keywords/
+    // ================================================================================
+    private fun join(keyword: String): JQLBuilder {
+        // validate
+        if (clauses.isNotEmpty()) clauses.add(keyword)
+        return this
+    }
+
+    fun and(): JQLBuilder {
+        return join(JOIN_KEYWORDS["and"]!!)
+    }
+
+    fun or(): JQLBuilder {
+        return join(JOIN_KEYWORDS["or"]!!)
+    }
+    // ================================================================================
+    //endregion
+
+    //region convenient helper
+    // ================================================================================
+    fun inProject(project: String): JQLBuilder {
+        return and().equal("project", project)
+    }
+
+    fun withIssueType(type: String): JQLBuilder {
+        return and().equal("type", type)
+    }
+
+    fun reportBy(user: String): JQLBuilder {
+        return and().equal("reporter", user)
+    }
+
+    fun assignTo(user: String): JQLBuilder {
+        return and().equal("assignee", user)
+    }
+    // ================================================================================
+    //endregion
+
+    fun build(): JQL {
+        var query = ""
+        clauses.forEach {
+            query = query.plus(" $it")
+        }
+        return JQL(query.trim())
+    }
+}
