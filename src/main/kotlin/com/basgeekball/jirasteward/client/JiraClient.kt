ceda6532bc67a5fd641943b48a58e5265f99de51
Jing Li <thyrlian@gmail.com>
2022-08-22 23:20:31 +0200

    Rename API from common to standard

--- src/main/kotlin/com/basgeekball/jirasteward/client/JiraClient.kt
+++ src/main/kotlin/com/basgeekball/jirasteward/client/JiraClient.kt
@@ -10,18 +10,18 @@ import org.springframework.web.bind.annotation.RequestMethod
 
 @FeignClient(value = "jira", configuration = [FeignClientConfiguration::class], url = "\${jira.host}")
 interface JiraClient {
-    @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.common}/serverInfo"])
+    @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.standard}/serverInfo"])
     fun getServerInfo() : ServerInfo
 
-    @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.common}/statuscategory"])
+    @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.standard}/statuscategory"])
     fun getStatusCategory() : List<Status.StatusCategory>
 
-    @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.common}/project"])
+    @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.standard}/project"])
     fun getProjects() : List<Project>
 
-    @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.common}/issue/{issueKey}"])
+    @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.standard}/issue/{issueKey}"])
     fun getIssue(@PathVariable("issueKey") issueKey: String) : Issue
 
-    @RequestMapping(method = [RequestMethod.POST], value = ["\${jira.api.common}/search"])
+    @RequestMapping(method = [RequestMethod.POST], value = ["\${jira.api.standard}/search"])
     fun getIssues(@RequestBody jql: JQL) : IssueHolder
 }
