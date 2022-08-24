bf9852095ed236a89a6a5fd6e4900569afbd800b
Jing Li <thyrlian@gmail.com>
2022-08-24 23:16:30 +0200

    Refactor get sprints endpoints

--- src/main/kotlin/com/basgeekball/jirasteward/controller/JiraController.kt
+++ src/main/kotlin/com/basgeekball/jirasteward/controller/JiraController.kt
@@ -7,10 +7,8 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 class JiraController {
@@ -18,7 +16,7 @@ class JiraController {
     private lateinit var jiraClient: JiraClient
     private val mapper = jacksonObjectMapper().configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
 
-    private fun getAllSprintsFromBoard(boardId: Int): List<Sprint> {
+    private fun getAllSprintsFromBoard(boardId: Int, state: String = ""): List<Sprint> {
         val sprints = mutableListOf<Sprint>()
         var isCompleted = false
         var startAt = 0
@@ -28,38 +26,21 @@ class JiraController {
             if (!isCompleted) startAt = sprintHolder.startAt!! + sprintHolder.maxResults!!
             sprints.addAll(sprintHolder.sprints!!)
         }
-        return sprints
-    }
-
-    private fun getSprintsByState(boardId: Int, state: String): List<Sprint> {
-        return getAllSprintsFromBoard(boardId).filter { it.state.equals(state) }
+        return if (state.isEmpty()) {
+            sprints
+        } else {
+            sprints.filter { it.state.equals(state) }
+        }
     }
 
     @GetMapping(value = ["/boards/{boardId}/sprints"], produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
-    fun getAllSprints(@PathVariable boardId: Int): ResponseEntity<String> {
-        val response = mapper.writeValueAsString(getAllSprintsFromBoard(boardId))
-        return ResponseEntity.ok(response)
-    }
-
-    @GetMapping(value = ["/boards/{boardId}/activesprints"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    @ResponseBody
-    fun getActiveSprints(@PathVariable boardId: Int): ResponseEntity<String> {
-        val response = mapper.writeValueAsString(getSprintsByState(boardId, "active"))
-        return ResponseEntity.ok(response)
-    }
-
-    @GetMapping(value = ["/boards/{boardId}/closedsprints"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    @ResponseBody
-    fun getClosedSprints(@PathVariable boardId: Int): ResponseEntity<String> {
-        val response = mapper.writeValueAsString(getSprintsByState(boardId, "closed"))
-        return ResponseEntity.ok(response)
-    }
-
-    @GetMapping(value = ["/boards/{boardId}/futuresprints"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    @ResponseBody
-    fun getFutureSprints(@PathVariable boardId: Int): ResponseEntity<String> {
-        val response = mapper.writeValueAsString(getSprintsByState(boardId, "future"))
+    fun getSprints(
+        @PathVariable boardId: Int,
+        @RequestParam("state") state: Optional<String>
+    ): ResponseEntity<String> {
+        val sprints = getAllSprintsFromBoard(boardId, state.orElse(""))
+        val response = mapper.writeValueAsString(sprints)
         return ResponseEntity.ok(response)
     }
 }
