34b92b9e54be1eac9b8730397cbfde2ac8f034e2
Jing Li <thyrlian@gmail.com>
2022-08-24 21:42:06 +0200

    Reformat code

--- src/main/kotlin/com/basgeekball/jirasteward/client/JiraClient.kt
+++ src/main/kotlin/com/basgeekball/jirasteward/client/JiraClient.kt
@@ -12,20 +12,24 @@ interface JiraClient {
     }
 
     @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.standard}/serverInfo"])
-    fun getServerInfo() : ServerInfo
+    fun getServerInfo(): ServerInfo
 
     @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.standard}/statuscategory"])
-    fun getStatusCategory() : List<Status.StatusCategory>
+    fun getStatusCategory(): List<Status.StatusCategory>
 
     @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.standard}/project"])
-    fun getProjects() : List<Project>
+    fun getProjects(): List<Project>
 
     @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.standard}/issue/{issueKey}"])
-    fun getIssue(@PathVariable("issueKey") issueKey: String) : Issue
+    fun getIssue(@PathVariable("issueKey") issueKey: String): Issue
 
     @RequestMapping(method = [RequestMethod.POST], value = ["\${jira.api.standard}/search"])
-    fun getIssues(@RequestBody jql: JQL) : IssueHolder
+    fun getIssues(@RequestBody jql: JQL): IssueHolder
 
     @RequestMapping(method = [RequestMethod.GET], value = ["\${jira.api.agile}/board/{boardId}/sprint"])
-    fun getSprints(@PathVariable("boardId") boardId: Int, @RequestParam(name = "startAt", required = false) startAt: Int = 0, @RequestParam(name = "maxResults", required = false) maxResults: Int = MAX_RESULTS) : SprintHolder
+    fun getSprints(
+        @PathVariable("boardId") boardId: Int,
+        @RequestParam(name = "startAt", required = false) startAt: Int = 0,
+        @RequestParam(name = "maxResults", required = false) maxResults: Int = MAX_RESULTS
+    ): SprintHolder
 }
