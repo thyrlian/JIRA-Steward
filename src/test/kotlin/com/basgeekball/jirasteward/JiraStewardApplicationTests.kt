44645d9a690b24565091a973901eec7172520c94
Jing Li <thyrlian@gmail.com>
2022-08-24 23:39:12 +0200

    Revert "Specify class to test"
    This reverts commit 09f9d7da7df6bb571e19b74550c10733580db8be.

--- src/test/kotlin/com/basgeekball/jirasteward/JiraStewardApplicationTests.kt
+++ src/test/kotlin/com/basgeekball/jirasteward/JiraStewardApplicationTests.kt
@@ -3,7 +3,7 @@ package com.basgeekball.jirasteward
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest(classes = [JiraStewardApplication::class])
+@SpringBootTest
 class JiraStewardApplicationTests {
 
 	@Test
