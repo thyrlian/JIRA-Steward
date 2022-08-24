48e3897b65d050d888de224674e52a88ce94b144
Jing Li <thyrlian@gmail.com>
2022-08-24 23:39:05 +0200

    Revert "Add test body"
    This reverts commit 41316549ebc6512209cc647714f476e3825feced.

--- src/test/kotlin/com/basgeekball/jirasteward/JiraStewardApplicationTests.kt
+++ src/test/kotlin/com/basgeekball/jirasteward/JiraStewardApplicationTests.kt
@@ -2,15 +2,12 @@ package com.basgeekball.jirasteward
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.ApplicationContext
-import kotlin.test.assertNotNull
 
 @SpringBootTest(classes = [JiraStewardApplication::class])
 class JiraStewardApplicationTests {
 
 	@Test
-	fun contextLoads(context: ApplicationContext) {
-		assertNotNull(context)
+	fun contextLoads() {
 	}
 
 }
